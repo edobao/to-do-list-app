c26b5bd77cfcd80c767798a81fdb3a16b3d26e71
Emily Dobao <e_dobao@outlook.com>
2020-07-27 23:56:32 -0400

    Initial Commit

---
+++ to-do-list-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  to-do-list-app
+//
+//  Created by Apple on 7/27/20.
+//  Copyright © 2020 Emily Dobao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
